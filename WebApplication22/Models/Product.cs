6975ab1bd31ed2cd17ce0459d3ceaf254b3e1f00
tnrao-trainer <tnrao.trainer@gmail.com>
2022-03-08 15:42:14 +0530

    Web api project creation

---
+++ WebApplication22/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication22.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+    }
+}
