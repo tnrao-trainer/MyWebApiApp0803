52cc32879e0a2f45a74b9652b8dabec83d494678
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2022-05-02 10:48:37 +0530

    Update ProductsController.cs

--- WebApplication22/Controllers/ProductsController.cs
+++ WebApplication22/Controllers/ProductsController.cs
@@ -20,7 +20,8 @@ namespace WebApplication22.Controllers
                 new Product(){ ProductId = 1023, ProductName = "Keyboard", UnitPrice = 1200, Quantity = 3 },
                 new Product(){ ProductId = 1024, ProductName = "iBall Mouse", UnitPrice = 890, Quantity = 4 },
                 new Product(){ ProductId = 1025, ProductName = "Boat Speakers", UnitPrice = 1900, Quantity = 6 },
-                new Product(){ ProductId = 1026, ProductName = "DLink Router", UnitPrice = 1350, Quantity = 2 }
+                new Product(){ ProductId = 1026, ProductName = "DLink Router", UnitPrice = 1350, Quantity = 2 },
+                new Product(){ ProductId = 1027, ProductName = "JBL Headset", UnitPrice = 2350, Quantity = 3 },
             };
 
             return Ok(prdList);
